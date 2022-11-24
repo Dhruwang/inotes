f7053cf3c147a659b816c2b0409faea004b9c230
Dhruwang <dhruwangjariwala18@gmail.com>
2022-11-24 22:31:18 +0530

    added password hashing

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,5 +17,4 @@ const UserSchema = new Schema({
   }
 });
 const User = mongoose.model('users',UserSchema)
-User.createIndexes()
 module.exports = User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,21 +2,33 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
-router.post('/',
+// create a user using: POST "api/auth/createuser" . No login required 
+router.post('/createUser',
     body('email','enter a valid email').isEmail(),
     body('password','enter a valid password').isLength({ min: 5 }),
-    (req, res) => {
+    async(req, res) => {
+        // if there are errors return bad request and errors 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        User.create({
+        // check whether the user with this email exist already 
+        let user = await User.findOne({email:req.body.email})
+        if(user){
+            return res.status(400).json({error:"sorry user with same email already exist"})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password,salt);
+
+        user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        }).then(user => res.json(user))
-        .catch(err=> {console.log(err)
-        res.json({error:"please enter valid values", message: err.message})})
+            password: secPass,
+        })
+        
+        res.json(user)
     })
 module.exports = router
