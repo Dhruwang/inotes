ef6e589362f2683bed4616acb0c9515339c933e6
Dhruwang <dhruwangjariwala18@gmail.com>
2022-12-03 17:57:15 +0530

    added CRUD in api

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -14,6 +14,7 @@ router.get('/fetchallnotes', fetchUser, async (req, res) => {
 
 })
 
+// endpoint to add notes. login required 
 router.post('/addnotes', fetchUser, [
     body('title',"enter a valid title").isLength({min:3}),
     body('description',"enter a valid description").isLength({min:3})
@@ -37,6 +38,50 @@ router.post('/addnotes', fetchUser, [
         res.status(401).json({ err: "internal server error" })
     }
 })
+// endpoint to update existing node  
+router.put('/updatenote/:id', fetchUser,async (req, res) => {
+    try { 
+        const{title,description,tag} = req.body
+        // creating a new note object 
+        let newNote = {}
+        if(title){newNote.title=title}
+        if(description){newNote.description=description}
+        if(tag){newNote.tag=tag}
 
+        // finding note to update 
+        let note = await Notes.findById(req.params.id);
+        if(!note){return res.status(404).send("note not found")}
+
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send('not allowed')
+        }
+        note = await Notes.findByIdAndUpdate(req.params.id,{$set : newNote},{new:true})
+        res.json({newNote})
+        }
+        
+     catch (error) {
+        console.log(error.message)
+        res.status(401).json({ err: "internal server error" })
+    }
+})
+// endpoint to delete an existing node  
+router.delete('/deletenode/:id', fetchUser,async (req, res) => {
+    try { 
+        // finding note to update 
+        let note = await Notes.findById(req.params.id);
+        if(!note){return res.status(404).send("note not found")}
+
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send('not allowed')
+        }
+        note = await Notes.findByIdAndDelete(req.params.id)
+        res.json({"Success":"node deleted",note:note})
+        }
+        
+     catch (error) {
+        console.log(error.message)
+        res.status(500).json({ err: "internal server error" })
+    }
+})
 module.exports = router
 
