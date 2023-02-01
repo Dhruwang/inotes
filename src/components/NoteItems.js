3197bf911f7106357a90993745ba7606e522b3d2
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-01 20:29:25 +0530

    added date amd time to notes pop inner

--- src/components/NoteItems.js
+++ src/components/NoteItems.js
@@ -1,12 +1,14 @@
 import React, { useContext, useState } from 'react'
 import NoteContext from '../context/notes/noteContext'
 import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function NoteItems(props) {
     const context = useContext(NoteContext)
     const { DeleteNotes } = context;
     const { note, updateNote } = props
     const [Toggle, setToggle] = useState(false)
+    const navigate = useNavigate();
 
     const optionToggle = () => {
         if (Toggle === true) {
@@ -19,6 +21,9 @@ export default function NoteItems(props) {
     const handleDelete =()=>{
         console.log("hello")
         props.showPopup("Delete", "are you sure you want to delete?",note._id)
+    }
+    const handleEdit = ()=>{
+        navigate(`note/${note._id}`)
     }
 
     return (
@@ -35,7 +40,7 @@ export default function NoteItems(props) {
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                         </svg>
                     </button>
-                    <button className='btn' id="edit" >
+                    <button className='btn' id="edit" onClick={handleEdit} >
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
@@ -49,15 +54,13 @@ export default function NoteItems(props) {
                 </div>}
                 <Link to={`note/${note._id}`}>
                     <div>
-
-
                         <div className="card-body">
                             <h5 className="card-title">{note.title}</h5>
                             <p className="card-text">{note.description}</p>
                             <i className="fa-solid fa-trash mx-2" onClick={() => { DeleteNotes(note._id) }}></i>
                             <i className="fa-solid fa-pen-to-square mx-2" onClick={() => { updateNote(note) }}></i>
                         </div>
-                        <div className='noteDate'>{note.createdAt.slice(0, 10)}</div>
+                        <div className='noteDate'>{note.createdAt.slice(0, 10)} </div>
                     </div>
 
                 </Link>
